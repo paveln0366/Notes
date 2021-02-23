949698ce8cb88c2a3e1f888634fa7f7aac7b57ce
Pavel <paveln0366@gmail.com>
2021-02-23 20:17:57 +0300

    AddNoteActivity created

--- app/src/main/java/com/pavelpotapov/notes/MainActivity.java
+++ app/src/main/java/com/pavelpotapov/notes/MainActivity.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerViewNotes;
-    private ArrayList<Note> notes = new ArrayList<>();
+    public static final ArrayList<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +21,23 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
 
-        notes.add(new Note("Title 1", "Description 1", "Day 1", 1));
-        notes.add(new Note("Title 2", "Description 2", "Day 2", 2));
-        notes.add(new Note("Title 3", "Description 3", "Day 3", 3));
-        notes.add(new Note("Title 4", "Description 4", "Day 4", 1));
-        notes.add(new Note("Title 5", "Description 5", "Day 5", 2));
-        notes.add(new Note("Title 6", "Description 6", "Day 6", 2));
-        notes.add(new Note("Title 7", "Description 7", "Day 7", 1));
+        if (notes.isEmpty()) {
+            notes.add(new Note("Title 1", "Description 1", "Day 1", 1));
+            notes.add(new Note("Title 2", "Description 2", "Day 2", 2));
+            notes.add(new Note("Title 3", "Description 3", "Day 3", 3));
+            notes.add(new Note("Title 4", "Description 4", "Day 4", 1));
+            notes.add(new Note("Title 5", "Description 5", "Day 5", 2));
+            notes.add(new Note("Title 6", "Description 6", "Day 6", 2));
+            notes.add(new Note("Title 7", "Description 7", "Day 7", 1));
+        }
 
         NotesAdapter adapter = new NotesAdapter(notes);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNotes.setAdapter(adapter);
+    }
+
+    public void onClickAddNote(View view) {
+        Intent intent = new Intent(this, AddNoteActivity.class);
+        startActivity(intent);
     }
 }
