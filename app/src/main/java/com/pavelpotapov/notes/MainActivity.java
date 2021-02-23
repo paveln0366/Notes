4089754b239281bf020126550f4181f4ee8d4ac6
Pavel <paveln0366@gmail.com>
2021-02-23 22:07:45 +0300

    Database created

--- app/src/main/java/com/pavelpotapov/notes/MainActivity.java
+++ app/src/main/java/com/pavelpotapov/notes/MainActivity.java
@@ -6,7 +6,10 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerViewNotes;
     public static final ArrayList<Note> notes = new ArrayList<>();
     private NotesAdapter adapter;
+    private NotesDBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +28,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
+        dbHelper = new NotesDBHelper(this);
 
-        if (notes.isEmpty()) {
-            notes.add(new Note("Title 1", "Description 1", "Day 1", 1));
-            notes.add(new Note("Title 2", "Description 2", "Day 2", 2));
-            notes.add(new Note("Title 3", "Description 3", "Day 3", 3));
-            notes.add(new Note("Title 4", "Description 4", "Day 4", 1));
-            notes.add(new Note("Title 5", "Description 5", "Day 5", 2));
-            notes.add(new Note("Title 6", "Description 6", "Day 6", 2));
-            notes.add(new Note("Title 7", "Description 7", "Day 7", 1));
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+//        if (notes.isEmpty()) {
+//            notes.add(new Note("Title 1", "Description 1", "Day 1", 1));
+//            notes.add(new Note("Title 2", "Description 2", "Day 2", 2));
+//            notes.add(new Note("Title 3", "Description 3", "Day 3", 3));
+//            notes.add(new Note("Title 4", "Description 4", "Day 4", 1));
+//            notes.add(new Note("Title 5", "Description 5", "Day 5", 2));
+//            notes.add(new Note("Title 6", "Description 6", "Day 6", 2));
+//            notes.add(new Note("Title 7", "Description 7", "Day 7", 1));
+//        }
+//
+//        for (Note note : notes) {
+//            ContentValues contentValues = new ContentValues();
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_TITLE, note.getTitle());
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_DESCRIPTION, note.getDescription());
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK, note.getDayOfWeek());
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_PRIORITY, note.getPriority());
+//            database.insert(NotesContract.NotesEntry.TABLE_NAME, null, contentValues);
+//        }
+
+        ArrayList<Note> notesFromDB = new ArrayList<>();
+        Cursor cursor = database.query(NotesContract.NotesEntry.TABLE_NAME, null, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            String title = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_TITLE));
+            String description = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DESCRIPTION));
+            String dayOfWeek = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK));
+            int priority = cursor.getInt(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_PRIORITY));
+            Note note = new Note(title, description, dayOfWeek, priority);
+            notesFromDB.add(note);
         }
+        cursor.close();
 
-        adapter = new NotesAdapter(notes);
+        adapter = new NotesAdapter(notesFromDB);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNotes.setAdapter(adapter);
         adapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
