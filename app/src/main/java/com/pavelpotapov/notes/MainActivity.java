c5032d9f5e84057faecfc8ea45a66cc952ccb851
Pavel <paveln0366@gmail.com>
2021-02-23 18:52:05 +0300

    RecyclerView with adapter created

--- app/src/main/java/com/pavelpotapov/notes/MainActivity.java
+++ app/src/main/java/com/pavelpotapov/notes/MainActivity.java
@@ -1,14 +1,34 @@
 package com.pavelpotapov.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewNotes;
+    private ArrayList<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
+
+        notes.add(new Note("Title 1", "Description 1", "Day 1", 1));
+        notes.add(new Note("Title 2", "Description 2", "Day 2", 2));
+        notes.add(new Note("Title 3", "Description 3", "Day 3", 3));
+        notes.add(new Note("Title 4", "Description 4", "Day 4", 1));
+        notes.add(new Note("Title 5", "Description 5", "Day 5", 2));
+        notes.add(new Note("Title 6", "Description 6", "Day 6", 2));
+        notes.add(new Note("Title 7", "Description 7", "Day 7", 1));
+
+        NotesAdapter adapter = new NotesAdapter(notes);
+        recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewNotes.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/pavelpotapov/notes/NotesAdapter.java
@@ -0,0 +1,57 @@
+package com.pavelpotapov.notes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHolder> {
+
+    private ArrayList<Note> notes;
+
+    public NotesAdapter(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
+        return new NotesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
+        Note note = notes.get(position);
+        holder.textViewTitle.setText(note.getTitle());
+        holder.textViewDescription.setText(note.getDescription());
+        holder.textViewDayOfWeek.setText(note.getDayOfWeek());
+        holder.textViewPriority.setText(String.format("%s", note.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    class NotesViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private TextView textViewDayOfWeek;
+        private TextView textViewPriority;
+
+        public NotesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewDescription = itemView.findViewById(R.id.textViewDescription);
+            textViewDayOfWeek = itemView.findViewById(R.id.textViewDayOfWeek);
+            textViewPriority = itemView.findViewById(R.id.textViewPriority);
+        }
+    }
+}
